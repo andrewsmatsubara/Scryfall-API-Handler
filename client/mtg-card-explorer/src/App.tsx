623443563300bb7bd079911480e2a389bd546d4d
Andrews Matsubara <andrewsmatsubara@gmail.com>
2023-07-20 23:19:35 -0300

    create client side typescript application template with create-react-app

---
+++ client/mtg-card-explorer/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      React Template
+    </div>
+  );
+}
+
+export default App;
--- server/README.md
+++ server/README.md
@@ -1,1 +1,1 @@
-Initial README!!!
+#Server
