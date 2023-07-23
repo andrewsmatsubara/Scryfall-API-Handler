e5f3ff297e44f28c92e0fe294bba57e9df40f2e9
Andrews Matsubara <andrewsmatsubara@gmail.com>
2023-07-23 20:54:25 -0300

    add documentation to App component

--- client/mtg-card-explorer/src/App.tsx
+++ client/mtg-card-explorer/src/App.tsx
@@ -1,7 +1,13 @@
-import React from 'react';
 import './App.css';
 import { CardSearchInput } from './components/CardSearchInput/CardSearchInput';
 
+/**
+ * main component
+ * 
+ * @return {JSX.Element}
+ * 
+ * @author Andrews Matsubara
+ */
 function App() {
   return (
     <div className="App">
